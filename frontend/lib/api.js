ad7e50921d4b8d081a5082248b6a552178e7a08a
unknown <adamnugraha339@gmail.com>
2026-05-09 10:05:32 +0700

    Final Fix: Connected Vercel to Live DevTunnels Backend

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,5 +1,5 @@
 // GANTI 5000 MENJADI 8080
-const BASE_URL = "http://localhost:8080/api/tasks";
+const API_BASE_URL = "https://dvlb2qxv-8080.asse.devtunnels.ms/api";
 
 export const getTasks = async () => {
   try {
